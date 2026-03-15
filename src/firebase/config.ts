fc66c8ced0ea2e9a2bfd8a5d0d27b5f56fdc25e8
Nicat Hüseynli <huseynlinicat2010@gmail.com>
2026-03-15 20:58:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4022139194-ced30",
+  "appId": "1:524141146997:web:cf320f44b3a6c41beb193c",
+  "apiKey": "AIzaSyBZtzmVL08UAfXxbrRxc1HxpEwmUVUorO4",
+  "authDomain": "studio-4022139194-ced30.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "524141146997"
+};
