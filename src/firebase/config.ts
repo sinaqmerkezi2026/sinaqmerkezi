75a8539ba1a18d570f0906be7dbf27878996ae7f
sinaqmerkezi2026 <sinaqmerkezi2026@gmail.com>
2026-03-19 19:29:02 +0400

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-4022139194-ced30",
   "appId": "1:524141146997:web:cf320f44b3a6c41beb193c",
-  "apiKey": "AIzaSyBZtzmVL08UAfXxbrRxc1HxpEwmUVUorO4",
+  "apiKey": "AIzaSyD8kDQZvenQ5uU1nxkvlyw2kP_ETY2iWzE",
   "authDomain": "studio-4022139194-ced30.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "524141146997"
